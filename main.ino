0f3d3cb7b8f2f141f1340c53557860df9c36b66c
alexis-pinon <pinon.alexis0gmail.com>
2019-03-14 15:48:15 +0100

    2 motors, motor gra position enslavement, adapted PI to position

--- main.ino
+++ main.ino
@@ -5,15 +5,24 @@
 
 
 #define LED_PIN 13
-#define ENC_A 19
-#define ENC_B 38
-#define PWM 8
-#define BI1 36
-#define BI2 37
+
+//moteur 1
+#define ENC_TEN_A 31
+#define ENC_TEN_B 18
+#define PWM_TEN 12
+#define BI1_TEN 34
+#define BI2_TEN 35
+
+// moteur 2
+#define ENC_GRA_A 19
+#define ENC_GRA_B 38
+#define PWM_GRA 8
+#define BI1_GRA 36
+#define BI2_GRA 37
 
 #define DT 100
-#define KP 0.001
-#define KI 0.0005
+#define KP 1
+#define KI 0.00001
 
 volatile int pos = 0; // interdit l'optimisation sur l'accès mémoire
 int vitesse = 80;
@@ -21,13 +30,16 @@ int vitesse = 80;
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN,OUTPUT);
-  pinMode(ENC_A, INPUT_PULLUP);
-  pinMode(ENC_B, INPUT_PULLUP);
-  pinMode(PWM, OUTPUT);
-  pinMode(BI1,OUTPUT);
-  pinMode(BI2,OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(ENC_A),count, RISING);
+  pinMode(ENC_GRA_A, INPUT_PULLUP);
+  pinMode(ENC_GRA_B, INPUT_PULLUP);
+  pinMode(PWM_GRA, OUTPUT);
+  pinMode(BI1_GRA,OUTPUT);
+  pinMode(BI2_GRA,OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ENC_GRA_A),count, RISING);
   Serial.begin(9600);
+
+  //Accordage
 }
 
 
@@ -38,44 +50,66 @@ void loop() {
   lastSampleTime=millis();
   float vit = 0;
   vit = motorSpeed();
-  float consigne = 300;
-  float erreur = consigne-vit;
-
+  float consigne = 20;
+  float erreur = consigne-pos;
   float commande = P_I(erreur);
-  moteur(commande);
+  moteur_GRA(commande);
 
-  Serial.println(vit);
+  Serial.println(pos);
 }
 
-void moteur(int valeur){
+void moteu_TEN(int valeur){
   if(valeur>0){
-    digitalWrite(BI1,0);
-    digitalWrite(BI2,1);
+    digitalWrite(BI1_TEN,0);
+    digitalWrite(BI2_TEN,1);
   } else {
-    digitalWrite(BI2,0);
-    digitalWrite(BI1,1);
+    digitalWrite(BI2_TEN,0);
+    digitalWrite(BI1_TEN,1);
   }
-  analogWrite(PWM, constrain(abs(valeur),0,191)); // constrain sécurise la valeur
+  analogWrite(PWM_TEN, constrain(abs(valeur),0,191)); // constrain sécurise la valeur
+}
+
+void moteur_GRA(int valeur){
+  if(valeur>0){
+    digitalWrite(BI1_GRA,0);
+    digitalWrite(BI2_GRA,1);
+  } else {
+    digitalWrite(BI2_GRA,0);
+    digitalWrite(BI1_GRA,1);
+  }
+  analogWrite(PWM_GRA, constrain(abs(valeur),0,191)); // constrain sécurise la valeur
+}
+
+void moteur_TEN(int valeur){
+  if(valeur>0){
+    digitalWrite(BI1_TEN,0);
+    digitalWrite(BI2_TEN,1);
+  } else {
+    digitalWrite(BI2_TEN,0);
+    digitalWrite(BI1_TEN,1);
+  }
+  analogWrite(PWM_TEN, constrain(abs(valeur),0,191)); // constrain sécurise la valeur
 }
 
 void count(){
-  if(digitalRead(ENC_B)){
+  if(digitalRead(ENC_GRA_B)){
     pos++;
   } else {
     pos--;
   }
 }
 
-float motorSpeed(){
-  static int pos_prec;
-  int pos_k;
-  noInterrupts(); // evite de lire une donnée en train d'être modifiée
-  pos_k = pos;
-  interrupts();
-  float vit = (float)(pos_k-pos_prec)*1000/DT;
-  pos_prec=pos_k;
-  return vit;
-}
+//float motorSpeed(){
+//  static int pos_prec;
+//  int pos_k;
+//  noInterrupts(); // evite de lire une donnée en train d'être modifiée
+//  pos_k = pos;
+//  interrupts();
+//  float vit = (float)(pos_k-pos_prec)*1000/DT;
+//  pos_prec=pos_k;
+//  return vit;
+//}
+
  float P_I(float erreur){
   static float x;
   float commande = 0;
